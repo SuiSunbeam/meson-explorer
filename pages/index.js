d3906239002c0dfa8d66a44bff5b943a0581669e
Phil <qft.gtr@gmail.com>
2022-02-13 03:24:12 -0800

    fix build errors

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,7 @@ function SwapRow({ swap }) {
             className='text-sm text-gray-900 hover:text-indigo-500 hover:underline'
             href={`${from.explorer}/token/${from.token.addr}`}
             target='_blank'
+            rel='noreferrer'
           >
             {from.networkAlias} {from.token.symbol}
           </a>
@@ -39,6 +40,7 @@ function SwapRow({ swap }) {
             className='text-sm text-gray-900 hover:text-indigo-500 hover:underline'
             href={`${to.explorer}/token/${to.token.addr}`}
             target='_blank'
+            rel='noreferrer'
           >
             {to.networkAlias} {to.token.symbol}
           </a>
@@ -51,14 +53,14 @@ function SwapRow({ swap }) {
       </td>
       <td className='px-3 py-4 whitespace-nowrap'>
         <span className='text-sm text-indigo-600 hover:text-indigo-500 hover:underline'>
-          <a href={`${from.explorer}/address/${swap.initiator}`} target='_blank'>
+          <a href={`${from.explorer}/address/${swap.initiator}`} target='_blank' rel='noreferrer'>
             {abbreviate(swap.initiator)}
           </a>
         </span>
       </td>
       <td className='px-3 py-4 whitespace-nowrap'>
         <span className='text-sm text-indigo-600 hover:text-indigo-500 hover:underline'>
-          <a href={`${to.explorer}/address/${swap.recipient}`} target='_blank'>
+          <a href={`${to.explorer}/address/${swap.recipient}`} target='_blank' rel='noreferrer'>
             {abbreviate(swap.recipient)}
           </a>
         </span>
@@ -73,18 +75,18 @@ export default function SwapList({ swaps, error }) {
   } else if (!swaps) {
     return (
       <div className='flex items-center justify-center mt-6'>
-        <svg class='animate-spin h-5 w-5 text-gray-500' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
-          <circle class='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' stroke-width='4'></circle>
-          <path class='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+        <svg className='animate-spin h-5 w-5 text-gray-500' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
+          <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+          <path className='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
         </svg>
       </div>
     )
   }
   return (
-    <div class='flex flex-col'>
-      <div class='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-        <div class='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-          <div class='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+    <div className='flex flex-col'>
+      <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+        <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+          <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
             <table className='min-w-full divide-y divide-gray-200'>
               <thead className='bg-gray-50'>
                 <tr>
--- pages/swap/[swapId].js
+++ pages/swap/[swapId].js
@@ -27,9 +27,9 @@ export default function Swap({ swapId, swap, error }) {
   } else if (!swap) {
     return (
       <div className='flex items-center justify-center mt-6'>
-        <svg class='animate-spin h-5 w-5 text-gray-500' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
-          <circle class='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' stroke-width='4'></circle>
-          <path class='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+        <svg className='animate-spin h-5 w-5 text-gray-500' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
+          <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+          <path className='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
         </svg>
       </div>
     )
@@ -55,7 +55,7 @@ export default function Swap({ swapId, swap, error }) {
           <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
             <dt className='text-sm font-medium text-gray-500'>From</dt>
             <dd className='mt-1 text-sm text-indigo-600 hover:text-indigo-500 hover:underline sm:mt-0 sm:col-span-2'>
-              <a href={`${from.explorer}/token/${from.token.addr}`} target='_blank'>
+              <a href={`${from.explorer}/token/${from.token.addr}`} target='_blank' rel='noreferrer'>
                 {from.networkName} {from.token.symbol}
               </a>
             </dd>
@@ -63,7 +63,7 @@ export default function Swap({ swapId, swap, error }) {
           <div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
             <dt className='text-sm font-medium text-gray-500'>To</dt>
             <dd className='mt-1 text-sm text-indigo-600 hover:text-indigo-500 hover:underline sm:mt-0 sm:col-span-2'>
-              <a href={`${to.explorer}/token/${to.token.addr}`} target='_blank'>
+              <a href={`${to.explorer}/token/${to.token.addr}`} target='_blank' rel='noreferrer'>
                 {to.networkName} {to.token.symbol}
               </a>
             </dd>
@@ -83,7 +83,7 @@ export default function Swap({ swapId, swap, error }) {
           <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
             <dt className='text-sm font-medium text-gray-500'>Initiator</dt>
             <dd className='mt-1 text-sm text-indigo-600 hover:text-indigo-500 hover:underline sm:mt-0 sm:col-span-2'>
-              <a href={`${from.explorer}/address/${swap.initiator}`} target='_blank'>
+              <a href={`${from.explorer}/address/${swap.initiator}`} target='_blank' rel='noreferrer'>
                 {swap.initiator}
               </a>
             </dd>
@@ -91,7 +91,7 @@ export default function Swap({ swapId, swap, error }) {
           <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
             <dt className='text-sm font-medium text-gray-500'>Recipient</dt>
             <dd className='mt-1 text-sm text-indigo-600 hover:text-indigo-500 hover:underline sm:mt-0 sm:col-span-2'>
-              <a href={`${to.explorer}/address/${swap.recipient}`} target='_blank'>
+              <a href={`${to.explorer}/address/${swap.recipient}`} target='_blank' rel='noreferrer'>
                 {swap.recipient}
               </a>
             </dd>
