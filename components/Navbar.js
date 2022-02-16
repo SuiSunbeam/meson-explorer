75795e0997b374d288906e0839ad01bb4db52b4e
Phil Li <qft.gtr@gmail.com>
2022-02-15 23:53:28 -0800

    Hide Disclosure.Button

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,14 +20,14 @@ export default function Navbar() {
             <div className='relative flex items-center justify-between h-16'>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className='inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                {/* <Disclosure.Button className='inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
                   <span className='sr-only'>Open main menu</span>
                   {open ? (
                     <XIcon className='block w-6 h-6' aria-hidden='true' />
                   ) : (
                     <MenuIcon className='block w-6 h-6' aria-hidden='true' />
                   )}
-                </Disclosure.Button>
+                </Disclosure.Button> */}
               </div>
               <div className='flex items-center justify-center flex-1 sm:items-stretch sm:justify-start'>
                 <div className='flex items-center flex-shrink-0'>
