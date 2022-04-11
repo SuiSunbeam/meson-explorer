c0b1cef210c7a88fb2dfb5c83b148fb6f7149257
Phil Li <qft.gtr@gmail.com>
2022-04-10 19:06:59 -0700

    Update index.js

--- components/TagNetworkToken/index.js
+++ components/TagNetworkToken/index.js
@@ -17,10 +17,15 @@ export default function TagNetworkToken ({ responsive, explorer, token }) {
   const href = `${explorer}/${token.link || `token/${token.addr}`}`
   return (
     <div className='flex items-center text-gray-500 hover:text-primary hover:underline cursor-pointer'>
-      <a href={href} className='flex itmes-center w-4 h-4 mr-1' target='_blank' rel='noreferrer'>
+      <a href={href} className='flex itmes-center w-4 h-4' target='_blank' rel='noreferrer'>
         {logo && <Image src={logo} alt='' />}
       </a>
-      <a href={href} className={classnames('text-xs', responsive ? 'hidden lg:flex' : 'flex')} target='_blank' rel='noreferrer'>
+      <a
+        href={href}
+        className={classnames('text-xs', responsive ? 'hidden lg:flex lg:ml-1' : 'flex ml-1')}
+        target='_blank'
+        rel='noreferrer'
+      >
         {token.symbol}
       </a>
     </div>
