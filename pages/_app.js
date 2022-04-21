c29c71b3d00f96f7ca96bb3dc4556e4175de96e7
iNorthIsle <ndobox@outlook.com>
2022-04-21 11:03:06 +0800

    feat: add GA support

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
+import Script from 'next/script'
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
 import Navbar from '../components/Navbar'
 import AppContext from '../lib/context'
+import * as ga from '../lib/ga'
 
 export default function App({ Component, pageProps }) {
   const [globalState, setGlobalState] = React.useState({})
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
       <Head>
         <title>Meson Explorer</title>
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-HWHFMSJDKH`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HWHFMSJDKH', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Navbar globalState={globalState} setGlobalState={setGlobalState} />
       <div className='flex-1 overflow-hidden'>
         <div className='h-full overflow-auto'>
