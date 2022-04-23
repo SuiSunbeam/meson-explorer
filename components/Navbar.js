fc406f1b9a5a8e626888d7624fd25dc50e6b55b8
Phil Li <qft.gtr@gmail.com>
2022-04-22 17:50:34 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { LinkIcon, CreditCardIcon } from '@heroicons/react/outline'
+import { UserCircleIcon } from '@heroicons/react/solid'
 
 import extensions from '../lib/extensions'
 import { presets, getExtType, abbreviate } from '../lib/swap'
@@ -191,7 +192,11 @@ function Profile () {
     <Menu as='div' className='ml-1 relative'>
       <div>
         <Menu.Button className='max-w-xs bg-gray-300 rounded-full flex items-center text-sm focus:outline-none'>
-          <img className='h-8 w-8 rounded-full' src={session?.avatar} alt='' />
+          {
+            session
+            ? <img className='h-8 w-8 rounded-full' src={session?.user.image} alt='' />
+            : <UserCircleIcon className='h-8 w-8 rounded-full bg-gray-100 text-gray-500'/>
+          }
         </Menu.Button>
       </div>
       <Transition
@@ -204,6 +209,7 @@ function Profile () {
         leaveTo='transform opacity-0 scale-95'
       >
         <Menu.Items className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 z-10'>
+          {session?.user && <div className='px-4 py-2 text-xs text-gray-500'>{session?.user.email}</div>}
           <Menu.Item>
             <div
               className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer'
