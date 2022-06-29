055030f1885c7a3bdf25fd023882549c976b0ed7
Phil Li <qft.gtr@gmail.com>
2022-06-29 18:12:11 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,15 +5,17 @@ export default withAuth(
   },
   {
     callbacks: {
-      authorized: ({ token }) => !!token,
-    },
+      authorized: ({ token }) => !!token
+    }
   }
 )
 
-export const config = { matcher: [
-  '/api/v1/swap/bonded',
-  '/api/v1/swap/locked',
-  '/api/v1/swap/conflict',
-  '/api/v1/stats/:path*',
-  '/api/v1/queued/:path*'
-] }
+export const config = {
+  matcher: [
+    '/api/v1/swap/bonded',
+    '/api/v1/swap/locked',
+    '/api/v1/swap/conflict',
+    '/api/v1/stats/:path*',
+    '/api/v1/queued/:path*'
+  ]
+}
