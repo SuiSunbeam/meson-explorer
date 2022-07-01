2fb23ba0a39d712271f98726b76f7af30248d3ce
Phil Li <qft.gtr@gmail.com>
2022-07-01 13:10:31 -0500

    cors

--- middleware.js
+++ middleware.js
@@ -1,21 +1,37 @@
+import { NextResponse } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
 
-export default withAuth(
-  function middleware(req) {
+export const middleware = withAuth(
+  async function middleware(req) {
+    const token = req.nextauth.token
+    const isAdmin = token?.roles?.includes('admin')
+    const pathname = req.nextUrl.pathname
+
+    if (!token && (
+      pathname.startsWith('/pending') ||
+      pathname.startsWith('/stats') ||
+      pathname.startsWith('/queued')
+    )) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+
+    if (!isAdmin && pathname.startsWith('/lp')) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+
+    if (!isAdmin && (
+      pathname.startsWith('/api/v1/swap/bonded') ||
+      pathname.startsWith('/api/v1/swap/locked') ||
+      pathname.startsWith('/api/v1/swap/conflict') ||
+      pathname.startsWith('/api/v1/stats') ||
+      pathname.startsWith('/api/v1/queued')
+    )) {
+      return NextResponse.rewrite(new URL('/api/401', req.url))
+    }
   },
   {
     callbacks: {
-      authorized: ({ token }) => token?.roles?.includes('admin')
+      authorized: () => true
     }
   }
 )
-
-export const config = {
-  matcher: [
-    '/api/v1/swap/bonded',
-    '/api/v1/swap/locked',
-    '/api/v1/swap/conflict',
-    '/api/v1/stats/:path*',
-    '/api/v1/queued/:path*'
-  ]
-}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,49 @@
 module.exports = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
+        ]
+      }
+    ]
+  }
 }
