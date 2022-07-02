1a3cc0f3a11224395928b1b83a03fa2a69c26591
Phil Li <qft.gtr@gmail.com>
2022-07-02 00:20:07 -0500

    Update [swapId].js

--- pages/api/v1/share/[swapId].js
+++ pages/api/v1/share/[swapId].js
@@ -42,9 +42,7 @@ export default async function handler(req, res) {
 
   const result = {
     address,
-    inChain: swap.inChain,
-    outChain: swap.outChain,
-    fee: swap.fee,
+    encoded: swap.encoded,
     duration,
     code: shareCode.code
   }
