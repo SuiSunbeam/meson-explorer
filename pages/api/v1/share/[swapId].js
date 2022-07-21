b56d5fb6ff5d3e9d29bd40ff577bad83007638c8
Phil Li <qft.gtr@gmail.com>
2022-07-21 18:43:35 -0500

    Update [swapId].js

--- pages/api/v1/share/[swapId].js
+++ pages/api/v1/share/[swapId].js
@@ -1,11 +1,16 @@
 import { Swaps, Shares, ShareCodes } from 'lib/db'
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    res.status(404).send()
-    return
+  if (req.method === 'POST') {
+    return await post(req, res)
+  } else if (req.method === 'PUT') {
+    return await put(req, res)
   }
 
+  res.status(404).send()
+}
+
+async function post(req, res) {
   const { swapId, locale = 'en' } = req.query
   const swap = await Swaps.findById(swapId)
   if (!swap) {
@@ -13,14 +18,14 @@ export default async function handler(req, res) {
     return
   }
 
-  let style = ''
+  const styles = []
   const duration = Math.floor((swap.released - swap.created) / 1000)
   if (swap.salt.charAt(4) === 'f') {
     if (swap.outChain !== '0x2328' || swap.amount < 100_000000) {
       res.status(400).json({ error: { code: -32602, message: 'Failed to create share code' } })
       return
     }
-    style = 'cashback-avax'
+    styles.push('cashback-avax')
   } else if (duration > 240 || duration < 20 || swap.fee > 2_000_000 || swap.inToken > 2) {
     res.status(400).json({ error: { code: -32602, message: 'Failed to create share code' } })
     return
@@ -34,6 +39,18 @@ export default async function handler(req, res) {
   if (!share) {
     res.status(400).json({ error: { code: -32602, message: 'Failed to create share code' } })
     return
+  }
+
+  if (swap.outChain === '0x0a0a') {
+    styles.push('aurora')
+  } else if (swap.outChain === '0x2329') {
+    styles.push('arbitrum')
+  }
+
+  if (['ar', 'fa'].includes(locale)) {
+    styles.push('rtl')
+  } else {
+    styles.push('default')
   }
 
   let shareCode = await ShareCodes.findById(swapId)
@@ -53,11 +70,16 @@ export default async function handler(req, res) {
 
   const result = {
     code: shareCode.code,
-    style,
+    style: styles[0],
+    styles,
     address,
     encoded: swap.encoded,
     duration,
   }
 
   res.json({ result })
+}
+
+async function put(req, res) {
+  res.status(404).send()
 }
