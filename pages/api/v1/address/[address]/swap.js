5e2b6024dce966a9d80c4aa74b457ac073956e34
Phil <qft.gtr@gmail.com>
2022-10-04 15:11:41 -0700

    create listHandler

--- pages/api/v1/address/[address]/swap.js
+++ pages/api/v1/address/[address]/swap.js
@@ -1,28 +1,9 @@
 import { Swaps } from 'lib/db'
+import { listHandler } from 'lib/api'
 
-export default async function handler(req, res) {
-  const address = req.query.address
-  const page = Number(req.query.page) || 0
-  const size = Number(req.query.size) || 10
-  if (page < 0 || page !== Math.floor(page)) {
-    res.status(400).json({ error: { code: -32602, message: 'Invalid page value' } })
-    return
-  } else if (size < 1 || size !== Math.floor(size)) {
-    res.status(400).json({ error: { code: -32602, message: 'Invalid size value' } })
-    return
-  } else if (size > 20) {
-    res.status(400).json({ error: { code: -32602, message: 'Size cannot exceed 20' } })
-    return
-  }
-
-  const query = { fromTo: address, disabled: { $ne: true } }
-  const total = await Swaps.count(query)
-  const list = await Swaps.find(query)
-    .select('encoded events initiator fromTo created released')
-    .sort({ created: -1 })
-    .limit(size)
-    .skip(size * page)
-    .exec()
-
-  res.json({ result: { total, list } })
-}
+export default listHandler({
+  collection: Swaps,
+  getQuery: req => ({ fromTo: req.query.address, disabled: { $ne: true } }),
+  sort: { created: -1 },
+  select: 'encoded events initiator fromTo created released'
+})
--- pages/api/v1/premium/index.js
+++ pages/api/v1/premium/index.js
@@ -1,26 +1,8 @@
 import { Premiums } from 'lib/db'
+import { listHandler } from 'lib/api'
 
-export default async function handler(req, res) {
-  const page = Number(req.query.page) || 0
-  const size = Number(req.query.size) || 10
-  if (page < 0 || page !== Math.floor(page)) {
-    res.status(400).json({ error: { code: -32602, message: 'Invalid page value' } })
-    return
-  } else if (size < 1 || size !== Math.floor(size)) {
-    res.status(400).json({ error: { code: -32602, message: 'Invalid size value' } })
-    return
-  } else if (size > 20) {
-    res.status(400).json({ error: { code: -32602, message: 'Size cannot exceed 20' } })
-    return
-  }
-
-  const query = { meta: { $exists: true } }
-  const total = await Premiums.count(query)
-  const list = await Premiums.find(query)
-    .sort({ 'meta.ts': -1 })
-    .limit(size)
-    .skip(size * page)
-    .exec()
-
-  res.json({ result: { total, list } })
-}
+export default listHandler({
+  collection: Premiums,
+  getQuery: () => ({ meta: { $exists: true } }),
+  sort: { 'meta.ts': -1 }
+})
