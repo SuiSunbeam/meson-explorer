9c5c85e089d681492ebd4b9e071e0f04ff4b7344
北屿一木 <ndobox@outlook.com>
2022-11-01 17:22:29 +0800

    refactor: cashback api

--- pages/api/v1/swap/cashBack.js
+++ pages/api/v1/swap/cashBack.js
@@ -1,50 +1,52 @@
-
 import { Banners, Swaps } from 'lib/db'
 import mesonPresets from '@mesonfi/presets'
 
 export default async function handler(req, res) {
-	if (req.method === 'GET') {
-		const result = await get()
-		if (result) {
-			res.json({ result })
-		} else {
-			res.status(404).send()
-		}
-	} else {
-		res.status(404).send()
-	}
+  if (req.method === 'GET') {
+    const result = await get()
+    if (result) {
+      res.json({ result })
+    } else {
+      res.status(404).send()
+    }
+  } else {
+    res.status(404).send()
+  }
 }
 
 async function get() {
-	const banner = await Banners.findOne({ text: "banner|cash-back", disabled: { $ne: false } })
-		.sort({ priority: -1 })
-		.select('priority disabled params')
-		.exec()
+  const banner = await Banners.findOne({ text: 'banner|cash-back', disabled: { $ne: false } })
+    .sort({ priority: -1 })
+    .select('params')
+    .exec()
 
-	const currentBanner = { ...banner._doc }
 
-	if (currentBanner) {
-		const { networkId, min, startDate, endDate } = currentBanner?.params
-		const shortSlip44ID = mesonPresets.getNetwork(networkId)?.shortSlip44
+  if (banner) {
+    const currentBanner = banner._doc
+    const { networkId, min, startDate, endDate } = currentBanner?.params || {}
+    const shortSlip44ID = mesonPresets.getNetwork(networkId)?.shortSlip44
 
-		const query = {
-			outChain: shortSlip44ID,
-			amount: {
-				$gte: min * 1000_000
-			},
-			created: {
-				$gt: new Date(startDate),
-				$lt: new Date(endDate)
-			}
-		}
+    if (!shortSlip44ID) {
+      return null
+    }
 
-		const rawList = await Swaps.find(query)
-			.select('encoded fromTo inChain inToken outChain outToken amount created executed fromTo')
-			.sort({ created: -1 })
-			.exec()
+    const query = {
+      outChain: shortSlip44ID,
+      amount: {
+        $gte: min * 1000_000
+      },
+      created: {
+        $gt: new Date(startDate),
+        $lt: new Date(endDate)
+      }
+    }
 
-		return rawList
-	} else {
-		return null
-	}
+    const rawList = await Swaps.find(query)
+      .select('encoded fromTo')
+      .exec()
+
+    return rawList
+  } else {
+    return null
+  }
 }
