dd826e652a69988a008a54e7367f411d2b9b1b30
Phil <qft.gtr@gmail.com>
2022-11-21 10:41:55 -0800

    add header roles

--- pages/api/v1/swap/index.js
+++ pages/api/v1/swap/index.js
@@ -3,9 +3,11 @@ import { listHandler } from 'lib/api'
 
 export default listHandler({
   collection: Swaps,
-  getQuery: (_, roles) => roles?.some(r => ['root', 'admin'].includes(r))
-    ? { disabled: { $ne: true } }
-    : { disabled: { $ne: true }, hide: { $ne: true } },
+  getQuery: (_, roles, headerRoles) => {
+    return roles?.some(r => ['root', 'admin'].includes(r)) || headerRoles.includes('data')
+      ? { disabled: { $ne: true } }
+      : { disabled: { $ne: true }, hide: { $ne: true } }
+  },
   sort: { created: -1 },
   select: 'encoded events initiator fromTo created released'
 })
