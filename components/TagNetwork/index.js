26bdf281b862830d1549aa1ac759c16c87dbd73c
Phil <qft.gtr@gmail.com>
2023-05-17 15:42:37 -0700

    Update TagNetwork

--- components/TagNetwork/index.js
+++ components/TagNetwork/index.js
@@ -4,37 +4,37 @@ import Image from 'next/image'
 import { ExternalIcon } from 'components/ExternalLink'
 import { getExplorerAddressLink } from 'lib/swap'
 
-import eth from './eth.png'
-import sepolia from './sepolia.png'
-import bnb from './bnb.png'
-import avax from './avax.png'
-import polygon from './polygon.png'
-import ftm from './ftm.png'
-import arb from './arb.png'
-import opt from './opt.png'
-import one from './one.png'
-import aurora from './aurora.png'
-import cfx from './cfx.png'
-import evmos from './evmos.png'
-import movr from './movr.png'
-import beam from './beam.png'
-import eos from './eos.png'
-import tron from './tron.png'
 import aptos from './aptos.png'
-import sui from './sui.png'
-import zksync from './zksync.png'
-import zkevm from './zkevm.png'
+import arb from './arb.png'
+import aurora from './aurora.png'
+import avax from './avax.png'
+import beam from './beam.png'
+import bnb from './bnb.png'
+import cfx from './cfx.png'
 import cronos from './cronos.png'
+import eos from './eos.png'
+import eth from './eth.png'
+import evmos from './evmos.png'
+import ftm from './ftm.png'
+import movr from './movr.png'
+import one from './one.png'
+import opt from './opt.png'
+import polygon from './polygon.png'
+import sepolia from './sepolia.png'
+import sui from './sui.png'
+import tron from './tron.png'
+import zkevm from './zkevm.png'
+import zksync from './zksync.png'
 
-const logos = { eth, goerli: eth, sepolia, bnb, avax, polygon, ftm, arb, opt, one, aurora, cfx, evmos, movr, beam, eos, tron, aptos, sui, zksync, zkevm, cronos }
+const icons = { eth, goerli: eth, sepolia, bnb, avax, polygon, ftm, arb, opt, one, aurora, cfx, evmos, movr, beam, eos, tron, aptos, sui, zksync, zkevm, cronos }
 
 export default function TagNetwork ({ responsive, size = 'sm', network, iconOnly, address, className }) {
   const id = network.id.split('-')[0]
-  const logo = logos[id]
+  const icon = icons[id]
   return (
     <div className={classnames('flex items-center text-gray-500', size === 'sm' && 'text-xs', className)}>
       <div className={classnames('flex items-center rounded-full shadow', size === 'md' ? 'w-5 h-5' : 'w-4 h-4')}>
-        {logo && <Image src={logo} alt='' />}
+        {icon && <Image src={icon} alt='' />}
       </div>
       {
         !iconOnly &&
