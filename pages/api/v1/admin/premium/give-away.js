c07a2300c9f77414c2081c6196c916bb9a390c25
Phil <qft.gtr@gmail.com>
2023-06-06 14:13:38 +0800

    Update give-away.js

--- pages/api/v1/admin/premium/give-away.js
+++ pages/api/v1/admin/premium/give-away.js
@@ -3,21 +3,27 @@ import { Banners } from 'lib/db'
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const banner = await Banners.findOne({ text: 'banner|free-premium' })
+    const banner = await Banners.findById('free-premium')
     if (banner) {
-      const redeemed = banner.meta.map(({ address }) => address)
-      const result = banner.address.map(address => redeemed.includes(address) ? { address, redeemed: true } : { address })
-      res.json({ result })
+      res.json({ result: banner.metadata })
     } else {
       res.status(400).json({ error: { code: -32602, message: 'Failed to get free premiums' } })
     }
   } else if (req.method === 'POST') {
-    const newAddress = req.body.filter(addr => utils.isAddress(addr)).map(addr => addr.toLowerCase())
-    await Banners.findOneAndUpdate({ text: 'banner|free-premium' }, [{ $addFields: { address: { $setUnion: [newAddress, '$address'] } } }])
+    const banner = await Banners.findById('free-premium')
+    if (!banner) {
+      res.status(400).json({ error: { code: -32602, message: 'Failed to get free premiums' } })
+      return
+    }
+    const newAddress = req.body
+      .map(addr => addr.toLowerCase())
+      .filter(addr => utils.isAddress(addr) && !banner.metadata.find(item => item.address === addr))
+      .map(address => ({ address }))
+    await Banners.findByIdAndUpdate('free-premium', { $addToSet: { metadata: { $each: newAddress } } })
     res.json({ result: true })
   } else if (req.method === 'DELETE') {
     const address = req.body.address.toLowerCase()
-    const result = await Banners.findOneAndUpdate({ text: 'banner|free-premium' }, { $pull: { address, meta: { address } } })
+    const result = await Banners.findByIdAndUpdate('free-premium', { $pull: { metadata: { address } } })
     res.json({ result })
   } else {
     res.status(404).send()
