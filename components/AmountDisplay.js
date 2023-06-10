e52207c2d4faf99412e5f85f1742d6a8cc883f74
Phil <qft.gtr@gmail.com>
2023-06-11 05:22:12 +0800

    show fee

--- components/AmountDisplay.js
+++ components/AmountDisplay.js
@@ -1,10 +1,12 @@
 import { ethers } from 'ethers'
 
-export default function AmountDisplay({ value, decimals = 6 }) {
+export default function AmountDisplay({ value, decimals = 6, size = 'sm' }) {
   const amount = ethers.utils.formatUnits(value, decimals)
   const [int, decimal] = amount.split('.')
   if (decimal?.length > 2) {
-    return <span>{int}.<span className='text-sm'>{decimal}</span></span>
+    return (
+      <span>{int}.<span className={size === 'xs' ? 'text-xs': 'text-sm'}>{decimal}</span></span>
+    )
   }
   return amount
 }
