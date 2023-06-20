f3bedbcd70a059f69c9c83b8c551a506934a2ca3
Phil <qft.gtr@gmail.com>
2023-06-20 13:30:23 +0800

    GET banner/[id]/check

---
+++ pages/api/v1/banner/[bannerId]/check.js
@@ -0,0 +1,33 @@
+import { Banners } from 'lib/db'
+
+function getBannerQuery (bannerId) {
+  const query = { disabled: { $ne: true } }
+  if (bannerId) {
+    query._id = bannerId
+  }
+  return query
+}
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    return await get(req, res)
+  }
+  res.status(404).send()
+}
+
+async function get(req, res) {
+  const { bannerId, address = '' } = req.query
+  const banner = await Banners.findOne(getBannerQuery(bannerId))
+    .sort({ priority: -1 })
+    .select('metadata')
+
+  if (!banner) {
+    res.json({ error: { code: 404, message: 'Not found' } })
+  }
+
+  if (banner.metadata.find(item => item.address === address.toLowerCase() )) {
+    res.json({ result: { isValid: true } })
+  } else {
+    res.json({ result: { isValid: false } })
+  }
+}
