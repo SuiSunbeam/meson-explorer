66e2df1a5ce64cb1284c893f99d497fb799c599a
Phil <qft.gtr@gmail.com>
2023-08-05 23:01:29 +0800

    failed filter

--- pages/api/v1/swap/index.js
+++ pages/api/v1/swap/index.js
@@ -8,7 +8,7 @@ export default listHandler({
     const query = { disabled: { $ne: true }, hide: { $ne: true } }
     if (roles?.some(r => ['root', 'admin'].includes(r)) || headerRoles.includes('data')) {
       delete query.hide
-      const { category, from, to } = req.query
+      const { category, from, to, failed } = req.query
       if (category === 'api') {
         query.salt = { $regex : /^0x[d9]/ }
         query['fromTo.0'] = { $ne: '0x666d6b8a44d226150ca9058beebafe0e3ac065a2' }
@@ -34,6 +34,9 @@ export default listHandler({
       }
       if (to) {
         query.outChain = presets.getNetwork(to).shortSlip44
+      }
+      if (failed) {
+        query['events.name'] = { $ne: 'RELEASED' }
       }
     }
     return query
