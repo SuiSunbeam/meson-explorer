0a1c3d7eb0a0e0a5133f2a7501ce5dc22ed6d0f1
Phil <qft.gtr@gmail.com>
2023-10-23 18:37:29 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './lib/**/*.{js,ts,jsx,tsx}',
   ],
+  safelist: [
+    'bg-warning',
+  ],
   theme: {
     extend: {
       fontFamily: {
