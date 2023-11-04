3ed01ffdc40cedf440e87043b20b4a75dd2264ed
Phil <qft.gtr@gmail.com>
2023-11-04 23:08:31 +0800

    Create index.js

---
+++ pages/api/v1/partner/intract/zkevm-10u/index.js
@@ -0,0 +1,24 @@
+import { Banners } from 'lib/db'
+
+function getBannerQuery (bannerId) {
+  const query = { disabled: { $ne: true } }
+  if (bannerId) {
+    query._id = bannerId
+  }
+  return query
+}
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { address } = req.body
+    const bannerId = 'zkevm-10u'
+    const banner = await Banners.findOne(getBannerQuery(bannerId))
+      .sort({ priority: -1 })
+      .select('metadata')
+
+    const result = !!banner.metadata.find(item => item.address === address.toLowerCase())
+    res.json({ result })
+  } else {
+    res.status(404).end()
+  }
+}
