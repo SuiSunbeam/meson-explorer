bda2e1782c6fcf49df94b40b7c21c6a3052a91c6
Phil <qft.gtr@gmail.com>
2023-12-04 21:45:06 +0800

    Update [addr].js

--- pages/api/v1/partner/eos/premium/[addr].js
+++ pages/api/v1/partner/eos/premium/[addr].js
@@ -1,8 +1,23 @@
 import fetch from 'node-fetch'
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { addr } = req.query
+  const { addr } = req.query
+  if (req.method === 'GET') {
+    const response = await fetch(`https://explorer.meson.fi/api/v1/premium/${addr}`)
+    const { result } = await response.json()
+    if (result?.records[0]) {
+      const { fromAddress, quota, since, until } = result.records[0]
+      const data = {
+        fromAddress,
+        quota: quota / 1e6,
+        since: new Date(since * 1000),
+        until: new Date(until * 1000),
+      }
+      res.json({ success: true, data })
+    } else {
+      res.json({ success: true, data: null })
+    }
+  } else if (req.method === 'POST') {
     const withFormat = `ethers:${addr.toLowerCase()}`
     const response = await fetch(`https://explorer.meson.fi/api/v1/premium/${withFormat}`, {
       method: 'POST'
