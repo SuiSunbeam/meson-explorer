654b419b00673d167cc4c2cc12bcbebba4b37e28
Phil <qft.gtr@gmail.com>
2023-12-16 12:25:20 +0800

    Update export.js

--- pages/api/v1/address/[address]/export.js
+++ pages/api/v1/address/[address]/export.js
@@ -1,6 +1,6 @@
 import { getToken } from 'next-auth/jwt'
 import { stringify } from 'csv-stringify/sync'
-import { ethers } from 'ethers'
+import { BigNumber, utils } from 'ethers'
 
 import { Swaps } from 'lib/db'
 import { presets, getStatusFromEvents } from 'lib/swap'
@@ -13,33 +13,42 @@ export default async function handler(req, res) {
     return
   }
 
-  const query = { fromTo: req.query.address, disabled: { $ne: true } }
+  const { address } = req.query
+  const query = { fromTo: address, disabled: { $ne: true } }
   const rawList = await Swaps.find(query)
     .select('encoded events fromTo created srFee lpFee')
-    .sort({ created: -1 })
+    .sort({ created: 1 })
     .exec()
   
+  let balance = BigNumber.from(0)
   const list = rawList.map(item => {
     const { _id, encoded, events, fromTo, created, srFee, lpFee } = item
     const { swap, from, to } = presets.parseInOutNetworkTokens(encoded)
     const status = getStatusFromEvents(events, swap.expireTs)
-    const amount = ethers.utils.formatUnits(swap.amount, 6)
-    const fee = ethers.utils.formatUnits((srFee || 0) + (lpFee || 0), 6)
+    const amount = utils.formatUnits(swap.amount, 6)
+    const fee = utils.formatUnits((srFee || 0) + (lpFee || 0), 6)
+    if (status === 'DONE') {
+      if (to.network.id === 'cfx' && fromTo[1] === address) {
+        balance = balance.add(swap.amount).sub((srFee || 0) + (lpFee || 0))
+      } else if (from.network.id === 'cfx' && fromTo[0] === address) {
+        balance = balance.sub(swap.amount)
+      }
+    }
     return [
       _id, new Date(created).toISOString(), status,
-      amount, fee,
+      amount, fee, utils.formatUnits(balance, 6),
       from.network.name, from.token.symbol, to.network.name, to.token.symbol,
       fromTo[0], fromTo[1]
     ]
   })
 
-  list.unshift([
-    'Swap ID', 'Created Time', 'Status', 'Amount', 'Fee',
+  list.push([
+    'Swap ID', 'Created Time', 'Status', 'Amount', 'Fee', 'Balance',
     'From', 'From Token', 'To', 'To Token',
     'Sender', 'Recipient'
   ])
 
-  const csv = stringify(list)
+  const csv = stringify(list.reverse())
 
   res.setHeader('Content-Type', 'text/csv')
   res.setHeader('Content-Disposition', `attachment; filename=meson_swaps_${req.query.address}.csv`)
